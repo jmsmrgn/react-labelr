1b038ff173ef21fcf869d499960de77d7be0f069
jmsmrgn <hello@jmsmrgn.com>
2016-01-20 11:33:21 -0800

    Create first component

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
-document.write('Hello Frontend Masters!')
+import React from 'react';
 
+const Zuul = React.createClass({
+  render() {
+    return <p>Are you the {this.props.name}?</p>
+  }
+});
+
+React.render(<Zuul name="gatekeeper"/>, document.body);
